a632ad8e658b20062db1699bdfb652d7bae439ad
Nidhi Shettigar <shettigarnidhi813@gmail.com>
2022-08-12 16:07:54 +0530

    init commit

---
+++ Comparision.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class Comparision{
+	public static void main(String[] args){
+		/*
+		String x = "Hello";
+		String y = "hello"; //string literal
+		String z = new String("hello"); //string object
+
+		System.out.println("Value of z:"+z);
+
+		if(x==y){
+			System.out.println("x==y");
+		}
+		else {
+			System.out.println("x!=y");
+		}
+
+		if(y==z){
+			System.out.println("y==z");
+		}
+		else {
+			System.out.println("y!=z");
+		}
+
+		if(y.equals(z)){
+			System.out.println("y====z");
+		}
+		else {
+			System.out.println("y!==z");
+		}
+		
+
+		int num1 = 3;
+		int num2 = 4;
+
+		if(num1==num2){
+			System.out.println("Equals");
+		}
+		else {
+			System.out.println("NOT Equals");
+		}
+
+		System.out.println(num1==num2?"Equals":"NOT Equals");
+		
+
+		String str1 = new String("Nidhi");
+		String str2 = new String("nidhi");
+		
+		System.out.println(str1==str2?"Equals":"NOT Equals");
+		if(str1.equals(str2)){
+			System.out.println("Equals");
+		}
+		else {
+			System.out.println("NOT Equals");
+		}
+		*/
+
+		boolean b1 = true;
+		boolean b2 = false;
+
+		System.out.println(b1&&b2);
+		System.out.println(b1||b2);
+	}
+}
---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.text.DecimalFormat;
+
+public class Main{
+	public static void main(String[] args){
+		
+	/*Scanner sc = new Scanner(System.in);
+
+	int id = sc.nextInt(); 
+	sc.nextLine();
+	String name = sc.nextLine();
+	char gender = sc.next().charAt(0);
+	float marks1 = sc.nextFloat();
+	float marks2 = sc.nextFloat();
+	double result = marks1+marks2;
+	float result1 = marks1+marks2;
+
+	System.out.println(id+"\t"+name+" "+gender+"\n"+result+" "+result1+"\n"+String.format("%.4f",result));
+	
+	//round off
+	double x = 89.976;
+
+	DecimalFormat dc = new DecimalFormat("#.#");
+	System.out.println("Roundoff:"+dc.format(x));
+
+	DecimalFormat dc1 = new DecimalFormat("#.##");
+	System.out.println("Roundoff:"+dc1.format(x));
+
+	//manual casting
+	double z = 45.56;
+	int a = (int)z;
+	System.out.println(z);
+	System.out.println(a);*/
+	
+	int num = 10;
+	System.out.println(num==10?"YES":"NO");
+	}
+}
